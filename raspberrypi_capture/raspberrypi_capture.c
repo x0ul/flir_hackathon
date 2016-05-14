e886386e51255fbf12e427eba34155e994e1e22b
John Vinyard <jvinyard@gmail.com>
2016-05-14 22:36:58 +0000

    Starting CS handling

--- raspberrypi_capture/raspberrypi_capture.c
+++ raspberrypi_capture/raspberrypi_capture.c
@@ -36,7 +36,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <limits.h>
 
 #include <time.h>
-#include <bcm2835.h>
+#include <wiringPi.h>
+
+#define FLIR_CS 6
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
@@ -50,7 +52,7 @@ static const char *device = "/dev/spidev0.1";
 static uint8_t mode;
 static uint8_t bits = 8;
 static uint32_t speed = 16000000;
-static uint16_t delay;
+static uint16_t my_delay;
 
 #define VOSPI_FRAME_SIZE (164)
 uint8_t lepton_frame_packet[VOSPI_FRAME_SIZE];
@@ -114,7 +116,7 @@ static void save_pgm_file(void)
 	for(i=0; i<60;i++) {
 		total += row_average[i];
 	}
-	printf("average = %u\n", total / 60);
+	printf("average = %lu\n", total / 60);
 	
 	printf("maxval = %u\n",maxval);
 	printf("minval = %u\n",minval);
@@ -136,14 +138,13 @@ static void save_pgm_file(void)
 int transfer(int fd)
 {
 	int ret;
-	int i;
-	int frame_number;
+	int frame_number = -1;
 	uint8_t tx[VOSPI_FRAME_SIZE] = {0, };
 	struct spi_ioc_transfer tr = {
 		.tx_buf = (unsigned long)tx,
 		.rx_buf = (unsigned long)lepton_frame_packet,
 		.len = VOSPI_FRAME_SIZE,
-		.delay_usecs = delay,
+		.delay_usecs = my_delay,
 		.speed_hz = speed,
 		.bits_per_word = bits,
 	};
@@ -158,7 +159,7 @@ int transfer(int fd)
 
 		if(frame_number < 60 )
 		{
-			for(i=0;i<80;i++)
+			for(int i=0;i<80;i++)
 			{
 				lepton_image[frame_number][i] = (lepton_frame_packet[2*i+4] << 8 | lepton_frame_packet[2*i+5]);
 			}
@@ -219,11 +220,15 @@ int main(int argc, char *argv[])
 	printf("bits per word: %d\n", bits);
 	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
 
+        digitalWrite(FLIR_CS, HIGH);
+        delay(200);
+        digitalWrite(FLIR_CS, LOW);
+        delay(100);
 	while(transfer(fd)!=59){}
 
 	close(fd);
 
-	save_pgm_file();
+//	save_pgm_file();
 
 	return ret;
 }
